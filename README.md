0bacb59b399f7a5807d826a76a8bcf255656936e
Suresh Ramani <ramani.suresh41@gmail.com>
2021-04-11 13:45:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# API Authentication using Laravel Sanctum
+
+### [Visit Blog](https://techvblogs.com/blog/api-authentication-laravel-sanctum?ref=repo)
+
+#### Install Packages
+
+```
+composer install
+```
+
+#### Copy .env file
+
+```
+cp .env.example .env
+```
+
+#### Set Database Detail
+
+#### Start Laravel Project
+
+```
+php artisan serve
+```
+
+# Connect with Us
+
+-   [Site](https://techvblogs.com/?ref=githubrepo)
+-   [Twitter](https://twitter.com/techvblogs)
+-   [Facebook](https://facebook.com/techvblogs)
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        if (!\Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function register(Request $request){
+        $post_data = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $post_data['name'],
+            'email' => $post_data['email'],
+            'password' => Hash::make($post_data['password']),
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
